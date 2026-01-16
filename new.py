f317aac8cf76f831087dbb6095bb571ca46abbea
anilrathod11 <118740702+anilrathod11@users.noreply.github.com>
2026-01-16 16:51:57 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,20 @@
+tes = 'curl -u "${Jfrog_USERNAME}:${Jfrog_PASSWORD}"'
+AZURE_KEY_VAULT_URL=https://agentic-ai-assist-dev.vault.a123.net/ 
+
+AZURE_CLIENT_ID="e234ebe2c-f8ec-2345-9962-2f524729768b"
+
+AZURE_TENANT_ID="a1a556a4-ef96-44a7-9887-1234567e6ey4" 
+
+REDACTED_SECRET
+
+  
+
+TENANT_ID="2132435jh-dd2c-4d1f-af13-5abe46b99921"
+
+CLIENT_ID="7f4e4477-1234-49cd-n234-dce140d6ec34"
+
+github_token = "ghp_zVuw8MJm2lIs0IuK66JoSXFQ72345rt"
+key = "sk-proj-30aUZFVfvDZ1vP6Dv_9y3Wwsedrtyhjf;gdlhwaerhjgkhjlewrthjk;ilkjfdhsytwterhgj"
+AUTOGEN_MODEL_TYPE="openai"
+DEVOPS_MCP_SERVER_URL="http://127.0.0.1:8012/devops/mcp"
+SECSCAN_MCP_SERVER_URL="http://127.0.0.1:8080/secops/mcp"
